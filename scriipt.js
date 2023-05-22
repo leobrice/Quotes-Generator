1f302636fce33eb6c372476170b72408ebc0d4f4
leobrice <mushibrayson07@gmail.com>
2023-05-22 09:14:10 +0300

    Final working with online data and offline paths added no script ;

---
+++ scriipt.js
@@ -0,0 +1,18 @@
+
+
+//     function ctrlShiftKey(e,keyCode){
+//         return e.ctrlShiftKey && e.ShiftKey && e.keyCode ===keyCode.charCodeAt(0);
+//     }
+// document.onkeydown =(e) =>{
+//     if (
+//         event.keyCode === 123 ||
+//         ctrlShiftKey(e,'I')    ||
+//         ctrlShiftKey(e,'j')    ||
+//         ctrlShiftKey(e,'c')    ||
+//         (e.ctrlKey && e.keyCode === 'u'.charCodeAt(0))
+//     ) 
+    
+//     return false;
+        
+    
+// }
